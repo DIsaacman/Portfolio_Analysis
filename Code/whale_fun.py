7828763d27819ab1255791216987b159bed92d44
Sydney Audio Visual Support <SydneyAudioVisualSup@mgd.mrshmc.com>
2022-07-11 15:32:04 +1000

    backup_work

--- Code/whale_fun.py
+++ Code/whale_fun.py
@@ -2,19 +2,25 @@
 
 
 #Takes in closing dates and closing price and creates a DataFrame with average daily returns
-def csvread_date(location_,column_):
-    
-    #Define path
-    ticker_data = Path(location_)
+def sheets_returns_csvread_date(location_,column_,name_):
+
+    # #Define path
+    # ticker_data = Path(location_)
     
     # Reading whale returns and set index with column_ object
-    ticker_df = pd.read_csv(ticker_data, index_col=column_, infer_datetime_format=True, parse_dates=True)
+    ticker_df = pd.read_csv(location_, index_col=column_, infer_datetime_format=True, parse_dates=True)
     
     # Sort by Date
     ticker_df.sort_index(inplace=True)
     
+    #Remove Null Columns
+    ticker_df.drop(columns=ticker_df.columns[[0,2,3]], axis=1, inplace=True)
+    
+    #Rename Close Price Column
+    ticker_df.rename(columns = {"Close":name_}, inplace = True)
+    
     #Average Return
-    ticker_df = ticker_df.pct_change().copy()
+    # ticker_df = ticker_df.pct_change().copy()
     
     #Drop Nulls
     ticker_df.dropna(inplace=True)
@@ -25,12 +31,13 @@ def csvread_date(location_,column_):
 
 
 def csvread_date(location_,column_):
-    
+    import pandas as pd
+
     #Define path
-    ticker_data = Path(location_)
+    # ticker_data = Path(location_)
     
     # Reading whale returns and set index with column_ object
-    ticker_df = pd.read_csv(ticker_data, index_col=column_, infer_datetime_format=True, parse_dates=True)
+    ticker_df = pd.read_csv(location_, index_col=column_, infer_datetime_format=True, parse_dates=True)
     
     # Sort by Date
     ticker_df.sort_index(inplace=True)
